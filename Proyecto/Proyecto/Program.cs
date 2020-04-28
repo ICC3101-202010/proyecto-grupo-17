e02db9b59bead1ff01bf5231b0b9a964f1bac11e
DanielMilstein <dnmilstein@miuandes.cl>
2020-04-28 16:35:39 -0400

    Proyecto creado con gitignore incl.

---
+++ Proyecto/Proyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
