9ccb9919018981656856ecd718d6207658699923
Jose Miguel <jmzegers@miuandes.cl>
2020-05-11 11:58:32 -0400

    Avance control

--- Proyecto/Proyecto/Control.cs
+++ Proyecto/Proyecto/Control.cs
@@ -15,7 +15,8 @@ namespace Proyecto
             Console.WriteLine("What are you looking for?");
             Console.WriteLine("a) A song");
             Console.WriteLine("b) A film");
-            Console.WriteLine("Type A or B");
+            Console.WriteLine("c) Both")
+            Console.WriteLine("Type A, B or C");
             string ans = Console.ReadLine();
 
 
@@ -25,6 +26,61 @@ namespace Proyecto
                 Console.WriteLine("a) Name");
                 Console.WriteLine("b) Artist");
                 Console.WriteLine("c) Album");
+                ans = Console.ReadLine();
+
+                List<Song> searchRes = new List<Song>();
+
+                if (ans == "A" || ans == "a")
+                {
+                    Console.WriteLine("Type the name of the song");
+                    string SongName = Console.ReadLine();
+
+                    List<Song> SFSongs = Spotflix.GetSongDB;
+
+                    foreach (Song s in SFSongs)
+                    {
+                        SongMetadata SMD = s.GetMetadata;
+                        string n1 = SMD.Name;
+
+                        if (n1 == SongName)
+                        {
+                            searchRes.Add(s);
+                        }
+                    }
+                }
+
+                else if (ans == "B" || ans == "b")
+                {
+                    Console.WriteLine("Type the name of the song artist");
+                    string ArtistName = Console.ReadLine();
+
+                    List<Song> SFSongs = Spotflix.GetSongDB;
+
+                    foreach (Song s in SFSongs)
+                    {
+                        SongMetadata SMD = s.GetMetadata;
+                        string n1 = SMD.Artist1.Name;
+
+                        if (a1 == ArtistName)
+                        {
+                            searchRes.Add(a1);
+                        }
+                    }
+                }
+
+
+                int results = searchRes.Count;
+                if (results == 0)
+                {
+                    Console.WriteLine("No results found!");
+                }
+
+                else
+                {
+                    Console.WriteLine(searchRes);
+                }
+
+
             }
 
 
@@ -33,7 +89,12 @@ namespace Proyecto
                 Console.WriteLine("What film element are you looking for?");
                 Console.WriteLine("a) Name");
                 Console.WriteLine("b) Director");
+            }
 
+
+            else
+            {
+                Console.WriteLine("Invalid answer");
             }
 
 
